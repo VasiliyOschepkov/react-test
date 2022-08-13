ff5142f7584f5a701706a6b88fd8d5e82d4de6fc
VasiliyOshchepkov <vasiliyoschepkov@gmail.com>
2022-08-13 12:03:15 +0300

    add components news, music, settings

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter} from "react-router-dom";
 import {Route, Routes} from "react-router";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 
@@ -18,6 +21,10 @@ function App() {
                     <Routes>
                         <Route path="/dialog" element={<Dialogs/>}/>
                         <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+
                     </Routes>
 
                 </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,18 +4,37 @@ import css from './Navbar.module.css'
 const Navbar = () => {
     return (
         <div className={css.nav}>
+
             <div className={`${css.item} ${css.active}`}>
-                <a href="/profile">Profile</a>
+                <a href="/profile">
+                    Profile
+                </a>
             </div>
+
             <div className={css.item}>
-                <a href="/dialog">Message</a>
+                <a href="/dialog">
+                    Message
+                </a>
             </div>
+
             <div className={css.item}>
-                <a>Musik</a>
+                <a href='/news'>
+                    News
+                </a>
             </div>
+
             <div className={css.item}>
-                <a>Settings</a>
+                <a href='/music'>
+                    Music
+                </a>
             </div>
+
+            <div className={css.item}>
+                <a href='/settings'>
+                    Settings
+                </a>
+            </div>
+
         </div>
     );
 }
